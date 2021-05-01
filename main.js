cd9cabdaece2fde0029888eb8c2a16ab3ecbcb6b
exequilodran <exequilodran@galiciaseguros.com.ar>
2021-04-30 22:32:06 -0300

    link templates

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("hola");
+'use strict';
+console.log("hello word :)");
+var modal_menu = document.getElementById('modal-menu');
+var menu_bars = document.getElementById("menu-bars");
+var menu_times = document.getElementById("menu-times");
+var isModalActive = false;
+
+menu_bars.addEventListener('click', function(){
+    console.log("in");
+    modal_menu.style.display = "block";
+    isModalActive = true;
+    handleScroll();
+})
+
+menu_times.addEventListener('click', function(){
+    console.log("out");
+    modal_menu.style.display = "none";
+    isModalActive = false;
+    handleScroll();
+})
+
+function handleScroll(){
+    if(isModalActive){
+       var body = document.getElementsByTagName('body');
+       body.classList.add("modal-open");
+    } else {
+        body.classList.remove("modal-open"); 
+    }
+}
