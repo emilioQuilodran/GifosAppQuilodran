a15fd7ce418f028e97d97f9d563694846239d08d
exequilodran <exequilodran@galiciaseguros.com.ar>
2021-05-03 20:18:48 -0300

    dark mode partial

--- main.js
+++ main.js
@@ -3,28 +3,45 @@ console.log("hello word :)");
 var modal_menu = document.getElementById('modal-menu');
 var menu_bars = document.getElementById("menu-bars");
 var menu_times = document.getElementById("menu-times");
-var isModalActive = false;
+var linkStyle = document.getElementById('theme-style');
+var theme_btn = document.getElementById('theme-btn');
+const DARK_PATH = "./styles/dark.css";
+let isModalActive = false;
+let isLight = true;
+
+let theme = localStorage.getItem('modo-nocturno');
 
 menu_bars.addEventListener('click', function(){
-    console.log("in");
     modal_menu.style.display = "block";
     isModalActive = true;
     handleScroll();
 })
-
 menu_times.addEventListener('click', function(){
-    console.log("out");
     modal_menu.style.display = "none";
     isModalActive = false;
     handleScroll();
 })
 
+theme_btn.addEventListener('click', modeSwitch);
+
 function handleScroll(){
     var body = document.getElementById('body');
-    console.log(body);
     if(isModalActive){
         body.classList.add("modal-open");
     } else {
         body.classList.remove("modal-open"); 
     }
+}
+function modeSwitch(){
+    isLight = !isLight;
+    if(isLight){
+        linkStyle.href = "";
+        theme_btn.innerHTML = "Modo Nocturno";
+        console.log(" theme dark: ",isLight);
+    } else {
+        console.log(" theme dark: ",isLight);
+        linkStyle.href = DARK_PATH;
+        theme_btn.innerHTML = "Modo Diurno";
+    }
+    localStorage.setItem('modo-nocturno', isLight);
 }
